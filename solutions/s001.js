b2c016b2a7409fb266e816091cf3dd866a0a09ab
jschneibel <jonathan.schneibel@gmail.com>
2019-04-26 18:47:45 +0200

    Add problem statements, add solution 2.

--- solutions/s001.js
+++ solutions/s001.js
@@ -1,7 +1,15 @@
 /*
+ * Solution to Project Euler problem 1 using JavaScript.
  * Copyright (c) 2019 Jonathan Schneibel
  * MIT License: https://opensource.org/licenses/MIT
  * GitHub: https://github.com/jschneibel/tiny-tune
+ */
+
+/*
+ * Problem 1:
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+ * get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
  */
 
 let sum = 0;
