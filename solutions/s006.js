6f8fe38b48f4bde2352c847f39e89db3e24a8e5f
jschneibel <37653547+jschneibel@users.noreply.github.com>
2019-04-27 14:46:32 +0200

    Add s006

---
+++ solutions/s006.js
@@ -0,0 +1,32 @@
+/*
+ * Solution to Project Euler problem 6 using JavaScript.
+ * Copyright (c) 2019 Jonathan Schneibel
+ * MIT License: https://opensource.org/licenses/MIT
+ * GitHub: https://github.com/jschneibel/tiny-tune
+ */
+
+/*
+ * Problem 6:
+ * The sum of the squares of the first ten natural numbers is,
+ * 1^2 + 2^2 + ... + 10^2 = 385
+ * The square of the sum of the first ten natural numbers is,
+ * (1 + 2 + ... + 10)^2 = 55^2 = 3025
+ * Hence the difference between the sum of the squares of the first ten natural
+ * numbers and the square of the sum is 3025 − 385 = 2640.
+ * Find the difference between the sum of the squares of the first one hundred
+ * natural numbers and the square of the sum.
+ */
+
+let squareOfSum = Math.pow((100 + 1) * 100 / 2, 2);
+
+let sumOfSquares = 0;
+
+for (let i = 1; i <= 100; i++) {
+  sumOfSquares += i*i;
+}
+
+let difference = squareOfSum - sumOfSquares;
+
+let solution = difference;
+
+console.log(solution);
